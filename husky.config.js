401cca7e1633f404acadf7a392c877e1c7a67058
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-09-12 13:54:23 +0100

    first commit

--- husky.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'npm run build',
-    'post-commit':
-      'echo -e \'\n-------------------------------------------------------- \n\\033[0;103mPLEASE MAKE SURE THE DOCS FOLDER IS ADDED AND COMMITTED \nCheck with " git status " \nIf in staging run " git add . " \nAnd " git commit -m "your msg" "\n-------------------------------------------------------- \n\' '
-  }
-};
