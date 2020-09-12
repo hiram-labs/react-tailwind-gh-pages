88db9825bf730cdb4c3787c1cb72b697303717a9
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-09-12 23:49:30 +0100

    first commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,7 +12,8 @@ const Header: React.FC<TProps> = (): JSX.Element => {
   return (
     <div className={cx(styles.container)}>
       <div className="text-center my-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        <h1>React Tailwind GH-Pages</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded-full">
           <a href="https://github.com/hiram-labs/react-tailwind-gh-pages#readme">
             Documentation
           </a>
